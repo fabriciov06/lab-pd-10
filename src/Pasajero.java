2565b360ca88e4bce1c0a870446fc1bf17d19956
Fabricio Vera <sebastianvt2006@gmail.com>
2026-06-14 15:15:58 -0500

    feat: Agregue los getters para cada atributo y modifique el tipo de dato de DNI como String

--- src/Pasajero.java
+++ src/Pasajero.java
@@ -1,12 +1,12 @@
 public class Pasajero {
     private String nombre;
-    private int dni;
+    private String dni;
     private String destino;
     private String fecha;
     private String hora;
     private int asiento;
 
-    public Pasajero(String nombre, int dni, String destino,
+    public Pasajero(String nombre, String dni, String destino,
                     String fecha, String hora, int asiento) {
         this.nombre = nombre;
         this.dni = dni;
@@ -14,5 +14,29 @@ public class Pasajero {
         this.fecha = fecha;
         this.hora = hora;
         this.asiento = asiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public int getAsiento() {
+        return asiento;
     }
 }
