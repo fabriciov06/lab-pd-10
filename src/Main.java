4881aca4f8174e21d611e615d53fb6527be53447
Fabricio Vera <sebastianvt2006@gmail.com>
2026-06-14 15:27:05 -0500

    feat: Cree la estructura HashMap para el almacenamiento de pasajeros

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
-public class Main {
+import java.util.HashMap;
 
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap<Integer, Pasajero> pasajeros =
+                new HashMap<>();
+
+    }
 }
